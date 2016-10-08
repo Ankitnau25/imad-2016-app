058f44b494b9c5d3552f0b86d4e0e7754493367f
Ankit Nautiyal <an7197@gmail.com>
2016-10-08 07:33:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ var articleone={
            </p>
     `
 };
-var htmltemplate=`<html>
+function createTemplate(data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate=`<html>
     <head>
         <title>
              ${title}
@@ -49,13 +54,15 @@ var htmltemplate=`<html>
     </body>
 </html>
 `;
+return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
