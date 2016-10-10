dd1a0c6e088233ed7763ef5621ef2e1ed02d8f94
Ankit Nautiyal <an7197@gmail.com>
2016-10-10 17:50:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName',function(req,res){
     //article name=article-one
     //articles{articleName}={} content object for article one
