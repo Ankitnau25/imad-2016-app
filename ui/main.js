ec32157bda1dc2358771b162a7581a554ff43953
Ankit Nautiyal <an7197@gmail.com>
2016-10-10 18:35:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,16 +10,28 @@ img.onclick=function(){
     }
     var interval =setInterval(moveRight,50);
    // img.style.marginLeft='100px';
-};*/
+};*/ 
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick = function (){
- //make a request to counter end point
- //capture the response and store it into a variable
- //render the variable in correct span
- counter=counter+1;
- var span=document.getElementById('count');
- span.innerHTML=counter.toString();
- 
+         //make a request to counter end point
+         var request=new XMLHttpRequest();
+         //capture the response and store it into a variable
+         request.onreadystatechange= function() {
+             if(request.readyState --- XMLHttpRequest.DONE)
+             {
+                 //take some action
+                 if(request.status --- 200){
+                     var counter=request.responseText;
+                     var span=document.getElementById('count');
+                     span.innerHTML=counter.toString();
+                     
+                 }
+             }
+             //not done yet
+         };
+ //make a request
+ request.open('GET','http://ankitnau25.imad.hasura-app.io/counter',true);
+ request.send(null);
 };
