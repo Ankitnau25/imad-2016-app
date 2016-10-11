2033c5d3fe26062c45e0b6a1ebe9ac7142476f56
Ankit Nautiyal <an7197@gmail.com>
2016-10-11 08:59:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/favicon.ico', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+  res.sendFile(path.join('favicon.ico'));
 });
 
 
