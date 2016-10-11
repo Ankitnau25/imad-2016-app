21f63ef8076f387ed6e29eb028ada50f42df0b20
Ankit Nautiyal <an7197@gmail.com>
2016-10-11 09:33:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -70,6 +70,6 @@ submit.onclick=function(){
  //make a request
   var nameInput = document.getElementById('name');
   var name = nameInput.value;
-  request.open('GET','http://ankitnau25.imad.hasura-app.io/submit-name?name='+name,true);
+  request.open('GET','http://ankitnau25.imad.hasura-app.io/submit-name?name=' + name,true);
   request.send(null);
 };
