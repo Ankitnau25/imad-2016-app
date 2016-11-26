aec3df3f81b248ccf4e2a254de9df9080b29ffba
Ankit Nautiyal <an7197@gmail.com>
2016-11-26 08:22:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/ui/blog.html', function (req, res) {
+app.get('/blog.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'blog.html'));
 });
 
